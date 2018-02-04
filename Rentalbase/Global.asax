5e17d883ad360c3c8f82b3e449dd67583bb9cf9e
Michael Quandt <mdquandt@hotmail.com>
2018-02-03 18:10:17 -0800

    Add project files.

---
+++ Rentalbase/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Rentalbase.MvcApplication" Language="C#" %>
---
+++ Rentalbase/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Rentalbase.Startup))]
+namespace Rentalbase
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
