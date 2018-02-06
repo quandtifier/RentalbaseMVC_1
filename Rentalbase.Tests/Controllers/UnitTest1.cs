c46f67aa5a6105591757e1681278f7a38d0546c5
Michael Quandt <mdquandt@hotmail.com>
2018-02-06 08:49:20 -0800

    Broken ass unit tests

---
+++ Rentalbase.Tests/Controllers/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rentalbase.Controllers;
+using Rentalbase.Models;
+
+namespace Rentalbase.Tests.Controllers
+{
+    [TestClass]
+    public class PropertyControllerTest
+    {
+        [TestMethod]
+        public void TestDetails()
+        {
+            PropertyController controller = new PropertyController();
+            ViewResult viewResult = controller.Details(1) as ViewResult;
+            Property result = (Property)viewResult.ViewData.Model;
+
+            Property property = new Property();
+            property.ID = 1;
+            property.Street = "38 Galvin Road";
+            property.City = "Seattle";
+            property.State = "WA";
+            property.Zip = 98181;
+            property.Value = 5000;
+            property.Description = "Need something here";
+
+            Assert.AreEqual(property.Street, result.Street);
+
+        }
+    }
+}
